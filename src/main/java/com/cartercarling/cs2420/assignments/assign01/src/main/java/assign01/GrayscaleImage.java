f16daab51d790705950c9aa7603c540292a758ca
Carter Carling <cartercarling37@gmail.com>
2025-01-14 15:07:03 -0700

    Finished assign01

--- src/main/java/com/cartercarling/cs2420/assignments/assign01/src/main/java/assign01/GrayscaleImage.java
+++ src/main/java/com/cartercarling/cs2420/assignments/assign01/src/main/java/assign01/GrayscaleImage.java
@@ -1,7 +1,7 @@
 package assign01;
 
 import javax.imageio.ImageIO;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
@@ -12,173 +12,256 @@ import java.net.URL;
  * Represents a grayscale (black and white) image as a 2D array of "pixel"
  * brightness values: 255 is "white", 127 is "gray", and 0 is "black" with
  * intermediate values in between.
- * 
+ *
  * @author CS 2420 course staff and Carter Carling
  * @version 01/09/2025
  */
+@SuppressWarnings("unused")
 public class GrayscaleImage {
 
-	private double[][] imageData; // array of pixel brightness values
+    private final double[][] imageData; // array of pixel brightness values
 
-	/**
-	 * Initializes an image from a 2D array of doubles. This constructor creates a
-	 * copy of the input array.
-	 * 
-	 * @param data initial pixel values
-	 * @throws IllegalArgumentException if the input array is empty or "jagged"
-	 *                                  meaning not all rows are the same length
-	 */
-	public GrayscaleImage(double[][] data) {
-		if(data.length == 0 || data[0].length == 0) {
-			throw new IllegalArgumentException("Image is empty");
-		}
+    /**
+     * Initializes an image from a 2D array of doubles. This constructor creates a
+     * copy of the input array.
+     *
+     * @param data initial pixel values
+     *
+     * @throws IllegalArgumentException if the input array is empty or "jagged"
+     *                                  meaning not all rows are the same length
+     */
+    @SuppressWarnings("ManualArrayCopy")
+    public GrayscaleImage(double[][] data) {
+        if (data.length == 0 || data[0].length == 0) {
+            throw new IllegalArgumentException("Image is empty");
+        }
 
         this.imageData = new double[data.length][data[0].length];
-		for(int row = 0; row < this.imageData.length; row++) {
-			if(data[row].length != this.imageData[row].length) {
-				throw new IllegalArgumentException("All rows must have the same length");
-			}
-			for(int col = 0; col < this.imageData[row].length; col++) {
+        for (int row = 0; row < this.imageData.length; row++) {
+            if (data[row].length != this.imageData[row].length) {
+                throw new IllegalArgumentException("All rows must have the same length");
+            }
+            for (int col = 0; col < this.imageData[row].length; col++) {
                 this.imageData[row][col] = data[row][col];
-			}
-		}
-	}
+            }
+        }
+    }
 
-	/**
-	 * Fetches an image from the specified URL and converts it to grayscale. Uses
-	 * the AWT Graphics2D class to do the conversion, so it may add an item to your
-	 * dock/menu bar as if you are loading a GUI program.
-	 * 
-	 * @param url from which to download the image
-	 * @throws IOException if the image cannot be downloaded for some reason
-	 */
-	public GrayscaleImage(URL url) throws IOException {
-		BufferedImage inputImage = ImageIO.read(url);
-		// Converts input image to grayscale based on information from
-		// https://stackoverflow.com/questions/6881578/how-to-convert-between-color-models
-		BufferedImage grayImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(),
-				BufferedImage.TYPE_BYTE_GRAY);
-		Graphics2D g2d = grayImage.createGraphics();
-		g2d.drawImage(inputImage, 0, 0, null);
-		g2d.dispose();
+    /**
+     * Fetches an image from the specified URL and converts it to grayscale. Uses
+     * the AWT Graphics2D class to do the conversion, so it may add an item to your
+     * dock/menu bar as if you are loading a GUI program.
+     *
+     * @param url from which to download the image
+     *
+     * @throws IOException if the image cannot be downloaded for some reason
+     */
+    public GrayscaleImage(URL url) throws IOException {
+        BufferedImage inputImage = ImageIO.read(url);
+        // Converts input image to grayscale based on information from
+        // https://stackoverflow.com/questions/6881578/how-to-convert-between-color-models
+        BufferedImage grayImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(),
+                BufferedImage.TYPE_BYTE_GRAY);
+        Graphics2D g2d = grayImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, null);
+        g2d.dispose();
         this.imageData = new double[grayImage.getHeight()][grayImage.getWidth()];
 
-		// Raster is basically a width x height x 1 3D array
-		WritableRaster grayRaster = grayImage.getRaster();
-		for(int row = 0; row < this.imageData.length; row++) {
-			for(int col = 0; col < this.imageData[0].length; col++) {
-				// getSample parameters are x (our column) and y (our row); i.e., "backwards"
+        // Raster is basically a width x height x 1 3D array
+        WritableRaster grayRaster = grayImage.getRaster();
+        for (int row = 0; row < this.imageData.length; row++) {
+            for (int col = 0; col < this.imageData[0].length; col++) {
+                // getSample parameters are x (our column) and y (our row); i.e., "backwards"
                 this.imageData[row][col] = grayRaster.getSampleDouble(col, row, 0);
-			}
-		}
-	}
-	public static void mirrored(GrayscaleImage image) {
+            }
+        }
+    }
 
-	}
+    /**
+     * Mirrors the given image
+     *
+     * @param image The image to mirror
+     */
+    public static void mirrored(GrayscaleImage image) {
+        int imageWidth = image.imageData[0].length;
+        int imageHeight = image.imageData.length;
 
-	// TODO: Add all specified methods below. Then delete this TODO comment.
-	/**
-	 * Saves the image as a PNG file.
-	 *
-	 * @param filename of the created image file
-	 * @throws IOException if the file cannot be written
-	 */
-	public void savePNG(File filename) throws IOException {
-		BufferedImage outputImage = new BufferedImage(this.imageData[0].length, this.imageData.length,
-				BufferedImage.TYPE_BYTE_GRAY);
-		WritableRaster raster = outputImage.getRaster();
-		for(int row = 0; row < this.imageData.length; row++) {
-			for(int col = 0; col < this.imageData[0].length; col++) {
-				raster.setSample(col, row, 0, this.imageData[row][col]);
-			}
-		}
-		ImageIO.write(outputImage, "png", filename);
-	}
-	/**
-	 * Gets the pixel brightness at the specified location
-	 * @param x The x coordinate of the pixel
-	 * @param y	The y coordinate of the pixel
-	 * @return The brightness value of the pixel
-	 */
-	public double getPixel(int x, int y) {
-		int maxWidth = this.imageData.length;
-		int maxHeight = this.imageData[0].length;
+        int centerX = imageWidth / 2;
 
-		if(y > maxWidth || x > maxHeight)
-			throw new IllegalArgumentException("Pixel out of bounds");
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < centerX; x++) {
+                int otherX = imageWidth - 1 - x;
 
-		return this.imageData[y][x];
-	}
-	/**
-	 * Sets the brightness value of the pixel at the specified location
-	 * @param x The x coordinate of the pixel
-	 * @param y The y coordinate of the pixel
-	 * @param brightnessValue The brightness value to set the pixel to
-	 */
-	public void setPixel(int x, int y, double brightnessValue) {
-		int maxWidth = this.imageData.length - 1;
-		int maxHeight = this.imageData[0].length - 1;
+                double current = image.getPixel(x, y);
+                double other = image.getPixel(otherX, y);
 
-		if(y > maxWidth || x > maxHeight)
-			throw new IllegalArgumentException("Pixel out of bounds");
+                image.setPixel(x, y, other);
+                image.setPixel(otherX, y, current);
+
+            }
+        }
+
+    }
+
+    /**
+     * Saves the image as a PNG file.
+     *
+     * @param filename of the created image file
+     *
+     * @throws IOException if the file cannot be written
+     */
+    public void savePNG(File filename) throws IOException {
+        BufferedImage outputImage = new BufferedImage(this.imageData[0].length, this.imageData.length,
+                BufferedImage.TYPE_BYTE_GRAY);
+        WritableRaster raster = outputImage.getRaster();
+        for (int row = 0; row < this.imageData.length; row++) {
+            for (int col = 0; col < this.imageData[0].length; col++) {
+                raster.setSample(col, row, 0, this.imageData[row][col]);
+            }
+        }
+        ImageIO.write(outputImage, "png", filename);
+    }
+
+    /**
+     * Gets the pixel brightness at the specified location
+     *
+     * @param x The x coordinate of the pixel
+     * @param y The y coordinate of the pixel
+     *
+     * @return The brightness value of the pixel
+     */
+    public double getPixel(int x, int y) {
+        int maxHeight = this.imageData.length;
+        int maxWidth = this.imageData[0].length;
+
+        if (y >= maxHeight || y < 0 || x >= maxWidth || x < 0)
+            throw new IllegalArgumentException("Pixel out of bounds");
+
+        return this.imageData[y][x];
+    }
+
+    /**
+     * Sets the brightness value of the pixel at the specified location
+     *
+     * @param x               The x coordinate of the pixel
+     * @param y               The y coordinate of the pixel
+     * @param brightnessValue The brightness value to set the pixel to
+     */
+    public void setPixel(int x, int y, double brightnessValue) {
+        int maxHeight = this.imageData.length;
+        int maxWidth = this.imageData[0].length;
+
+        if (y >= maxHeight || y < 0 || x >= maxWidth || x < 0)
+            throw new IllegalArgumentException("Pixel out of bounds");
+
+        if (brightnessValue > 255 || brightnessValue < 0)
+            throw new IllegalArgumentException("Brightness value out of bounds");
 
         this.imageData[y][x] = brightnessValue;
-	}
-	/**
-	 * Checks if other object is equal to current one
-	 * @param other The object to compare with
-	 * @return True if the objects are equal, false if not.
-	 */
-	public boolean equals(Object other) {
-		if(!(other instanceof GrayscaleImage otherImage))
-			return false;
+    }
+
+    /**
+     * Checks if other object is equal to current one
+     *
+     * @param other The object to compare with
+     *
+     * @return True if the objects are equal, false if not.
+     */
+    public boolean equals(Object other) {
+        if (!(other instanceof GrayscaleImage otherImage))
+            return false;
 
         boolean sameSize = this.imageData.length == otherImage.imageData.length && this.imageData[0].length == otherImage.imageData[0].length;
 
-		if(!sameSize)
-			return false;
+        if (!sameSize)
+            return false;
 
-		boolean allPixelsSame = true;
+        boolean allPixelsSame = true;
 
-		for(int x = 0; x < this.imageData.length; x++) {
-			for(int y = 0; y < this.imageData[0].length; y++) {
-				if(this.getPixel(x, y) != otherImage.getPixel(x, y)) {
-					allPixelsSame = false;
-					break;
-				}
-			}
-		}
+        int imageWidth = this.imageData[0].length;
+        int imageHeight = this.imageData.length;
 
-		return allPixelsSame;
-	}
-	/**
-	 * Gets the average brightness of the image
-	 * @return The average brightness of the image
-	 */
-	public double averageBrightness() {
-		int imageWidth = this.imageData.length;
-		int imageHeight = this.imageData[0].length;
-		int totalPixels = imageWidth * imageHeight;
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < imageWidth; x++) {
+                if (this.getPixel(x, y) != otherImage.getPixel(x, y)) {
+                    allPixelsSame = false;
+                    break;
+                }
+            }
+        }
 
-		double totalBrightnessValue = 0;
-		for(int x = 0; x < imageWidth; x++) {
-			for(int y = 0; y < imageWidth; y++) {
-				totalBrightnessValue += this.getPixel(x, y);
-			}
-		}
+        return allPixelsSame;
+    }
 
-		return totalBrightnessValue / totalPixels;
-	}
+    /**
+     * Gets the average brightness of the image
+     *
+     * @return The average brightness of the image
+     */
+    public double averageBrightness() {
+        int imageWidth = this.imageData[0].length;
+        int imageHeight = this.imageData.length;
+        int totalPixels = imageWidth * imageHeight;
 
-	public GrayscaleImage normalized() {
-		
+        double totalBrightnessValue = 0;
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < imageWidth; x++) {
+                totalBrightnessValue += this.getPixel(x, y);
+            }
+        }
 
-		return this;
-	}
+        return totalBrightnessValue / totalPixels;
+    }
 
-	public void invert() {
+    /**
+     * Creates a new GrayscaleImage with an average brightness value of 127
+     *
+     * @return a GrayscaleImage with an average brightness of 127
+     *
+     * @see <a href="https://en.wikipedia.org/wiki/Feature_scaling#Methods">Feature scaling (Wikipedia)</a>
+     */
+    public GrayscaleImage normalized() {
+        int desiredAverage = 127;
+        double averageBrightness = this.averageBrightness();
 
-	}
+        double scaleFactor = desiredAverage / averageBrightness;
 
+        int imageWidth = this.imageData[0].length;
+        int imageHeight = this.imageData.length;
+
+        double[][] newImageData = new double[imageHeight][imageWidth];
+
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < imageWidth; x++) {
+                newImageData[y][x] = this.getPixel(x, y) * scaleFactor;
+            }
+        }
+
+        return new GrayscaleImage(newImageData);
+    }
+
+    /**
+     * Inverts the brightness of the image
+     */
+    public void invert() {
+        int imageWidth = this.imageData[0].length;
+        int imageHeight = this.imageData.length;
+
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < imageWidth; x++) {
+                double currentValue = this.imageData[y][x];
+                this.imageData[y][x] = 255 - currentValue;
+            }
+        }
+    }
+
+    /**
+     * Creates an exact copy of the current object
+     *
+     * @return A copy of the current object
+     */
+    public GrayscaleImage copy() {
+        return new GrayscaleImage(this.imageData);
+    }
 
 }
--- src/main/java/com/cartercarling/cs2420/assignments/assign01/src/main/java/assign01/GrayscaleImageTest.java
+++ src/main/java/com/cartercarling/cs2420/assignments/assign01/src/main/java/assign01/GrayscaleImageTest.java
@@ -3,81 +3,246 @@ package assign01;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * For testing the GrayscaleImage class.
- * 
+ *
  * @author CS 2420 course staff and Carter Carling
  * @version 01/09/2025
  */
 public class GrayscaleImageTest {
 
-	private GrayscaleImage smallSquare;
-	private GrayscaleImage smallWide;
+    private GrayscaleImage smallSquare;
+    private GrayscaleImage smallWide;
+    private GrayscaleImage smallBright;
+    private GrayscaleImage smallMedium;
 
-	/**
-	 * A helper method that checks each pixel against an expected array of values.
-	 * This assumes that the getPixel method is working correctly.
-	 * 
-	 * @param expected array of values
-	 * @param actual GrayscaleImage to compare to the expected values
-	 */
-	private void assertPixelValuesEqual(double[][] expected, GrayscaleImage actual) {
-		for(int row = 0; row < expected.length; row++) {
-			for(int col = 0; col < expected[0].length; col++) {
-				assertEquals(expected[row][col], actual.getPixel(col, row), expected[row][col] * .0001,
-						"pixel at row: " + row + " col: " + col + " incorrect");
-			}
-		}
-	}
+    /**
+     * A helper method that checks each pixel against an expected array of values.
+     * This assumes that the getPixel method is working correctly.
+     *
+     * @param expected array of values
+     * @param actual   GrayscaleImage to compare to the expected values
+     */
+    private void assertPixelValuesEqual(double[][] expected, GrayscaleImage actual) {
+        for (int row = 0; row < expected.length; row++) {
+            for (int col = 0; col < expected[0].length; col++) {
+                assertEquals(expected[row][col], actual.getPixel(col, row), expected[row][col] * .0001,
+                        "pixel at row: " + row + " col: " + col + " incorrect");
+            }
+        }
+    }
 
-	@BeforeEach
-	public void setUp() {
-        this.smallSquare = new GrayscaleImage(new double[][] { { 1, 2 }, { 3, 4 } });
-        this.smallWide = new GrayscaleImage(new double[][] { { 1, 2, 3 }, { 4, 5, 6 } });
-	}
+    @BeforeEach
+    public void setUp() {
+        this.smallSquare = new GrayscaleImage(new double[][]{ { 1, 2 }, { 3, 4 } });
+        this.smallWide = new GrayscaleImage(new double[][]{ { 1, 2, 3 }, { 4, 5, 6 } });
+        this.smallBright = new GrayscaleImage(new double[][]{ { 255, 254 }, { 253, 252 } });
+        this.smallMedium = new GrayscaleImage(new double[][]{ { 127, 128 }, { 129, 130 } });
+    }
 
-	@Test
-	public void testGetPixel() {
-		assertEquals(1, this.smallWide.getPixel(0, 0));
-		assertEquals(2, this.smallWide.getPixel(1, 0));
-		assertEquals(3, this.smallWide.getPixel(2, 0));
-		assertEquals(4, this.smallWide.getPixel(0, 1));
-		assertEquals(5, this.smallWide.getPixel(1, 1));
-		assertEquals(6, this.smallWide.getPixel(2, 1));
-	}
+    @Test
+    public void testGetPixel() {
+        assertEquals(1, this.smallWide.getPixel(0, 0));
+        assertEquals(2, this.smallWide.getPixel(1, 0));
+        assertEquals(3, this.smallWide.getPixel(2, 0));
+        assertEquals(4, this.smallWide.getPixel(0, 1));
+        assertEquals(5, this.smallWide.getPixel(1, 1));
+        assertEquals(6, this.smallWide.getPixel(2, 1));
 
-	@Test
-	public void testEqualsSelf() {
-		assertEquals(this.smallSquare, this.smallSquare, "Image was not equal to itself");
-	}
+        assertThrows(IllegalArgumentException.class, () -> this.smallWide.getPixel(3, 1));
+        assertThrows(IllegalArgumentException.class, () -> this.smallWide.getPixel(0, 4));
+        assertThrows(IllegalArgumentException.class, () -> this.smallWide.getPixel(0, -1));
+        assertThrows(IllegalArgumentException.class, () -> this.smallWide.getPixel(-1, -1));
+        assertThrows(IllegalArgumentException.class, () -> this.smallWide.getPixel(1, 2));
+    }
 
-	@Test
-	public void testEqualsEquivalent() {
-		GrayscaleImage equivalent = new GrayscaleImage(new double[][] { { 1, 2 }, { 3, 4 } });
-		assertEquals(this.smallSquare, equivalent, "Image was not equal to a distinct but equivalent image");
-	}
+    @Test
+    public void testSetPixel() {
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            smallSquareCopy.setPixel(0, 0, 10);
+            double[][] expected = new double[][]{ { 10, 2 }, { 3, 4 } };
+            this.assertPixelValuesEqual(expected, smallSquareCopy);
+        }
 
-	@Test
-	public void testNormalized() {
-		GrayscaleImage smallNorm = this.smallSquare.normalized();
-		double scale = 127 / 2.5;
-		double[][] expected = new double[][] { { scale, 2 * scale }, { 3 * scale, 4 * scale } };
-        this.assertPixelValuesEqual(expected, smallNorm);
-	}
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            smallSquareCopy.setPixel(1, 0, 10);
+            double[][] expected = new double[][]{ { 1, 10 }, { 3, 4 } };
+            this.assertPixelValuesEqual(expected, smallSquareCopy);
+        }
 
-	@Test
-	public void testInvert() {
-        this.smallSquare.invert();
-		double[][] expected = new double[][] { { 254, 253 }, { 252, 251 } };
-        this.assertPixelValuesEqual(expected, this.smallSquare);
-	}
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            smallSquareCopy.setPixel(0, 1, 10);
+            double[][] expected = new double[][]{ { 1, 2 }, { 10, 4 } };
+            this.assertPixelValuesEqual(expected, smallSquareCopy);
+        }
 
-	@Test
-	public void testMirrored() {
-		double[][] expected = new double[][] { { 2, 1 }, { 4, 3 } };
-		GrayscaleImage.mirrored(this.smallSquare);
-        this.assertPixelValuesEqual(expected, this.smallSquare);
-	}
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            smallSquareCopy.setPixel(1, 1, 10);
+            double[][] expected = new double[][]{ { 1, 2 }, { 3, 10 } };
+            this.assertPixelValuesEqual(expected, smallSquareCopy);
+        }
+
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            smallSquareCopy.setPixel(0, 0, 0);
+            double[][] expected = new double[][]{ { 0, 2 }, { 3, 4 } };
+            this.assertPixelValuesEqual(expected, smallSquareCopy);
+        }
+
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            assertThrows(IllegalArgumentException.class, () -> smallSquareCopy.setPixel(1, 2, 1));
+        }
+
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            assertThrows(IllegalArgumentException.class, () -> smallSquareCopy.setPixel(1, 2, 1));
+        }
+
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            assertThrows(IllegalArgumentException.class, () -> smallSquareCopy.setPixel(2, 2, 1));
+        }
+
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            assertThrows(IllegalArgumentException.class, () -> smallSquareCopy.setPixel(-1, 0, 1));
+        }
+
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            assertThrows(IllegalArgumentException.class, () -> smallSquareCopy.setPixel(1, 1, 256));
+        }
+
+        {
+            GrayscaleImage smallSquareCopy = this.smallSquare.copy();
+            assertThrows(IllegalArgumentException.class, () -> smallSquareCopy.setPixel(1, 2, -1));
+        }
+
+    }
+
+    @SuppressWarnings("EqualsWithItself")
+    @Test
+    public void testEqualsSelf() {
+        assertEquals(this.smallSquare, this.smallSquare, "Image was not equal to itself");
+        assertEquals(this.smallWide, this.smallWide, "Image was not equal to itself");
+        assertEquals(this.smallBright, this.smallBright, "Image was not equal to itself");
+        assertEquals(this.smallMedium, this.smallMedium, "Image was not equal to itself");
+    }
+
+    @Test
+    public void testEqualsEquivalent() {
+        {
+            GrayscaleImage equivalent = new GrayscaleImage(new double[][]{ { 1, 2 }, { 3, 4 } });
+            assertEquals(this.smallSquare, equivalent, "Image was not equal to a distinct but equivalent image");
+        }
+
+        {
+            GrayscaleImage equivalent = new GrayscaleImage(new double[][]{ { 1, 2, 3 }, { 4, 5, 6 } });
+            assertEquals(this.smallWide, equivalent, "Image was not equal to a distinct but equivalent image");
+        }
+
+        {
+            GrayscaleImage equivalent = new GrayscaleImage(new double[][]{ { 255, 254 }, { 253, 252 } });
+            assertEquals(this.smallBright, equivalent, "Image was not equal to a distinct but equivalent image");
+        }
+
+        {
+            GrayscaleImage equivalent = new GrayscaleImage(new double[][]{ { 127, 128 }, { 129, 130 } });
+            assertEquals(this.smallMedium, equivalent, "Image was not equal to a distinct but equivalent image");
+        }
+    }
+
+    @Test
+    public void testNormalized() {
+        {
+            GrayscaleImage norm = this.smallSquare.normalized();
+            double scale = 127 / 2.5;
+            double[][] expected = new double[][]{ { scale, 2 * scale }, { 3 * scale, 4 * scale } };
+            this.assertPixelValuesEqual(expected, norm);
+        }
+
+        {
+            GrayscaleImage norm = this.smallWide.normalized();
+            double scale = 127 / 3.5;
+            double[][] expected = new double[][]{ { scale, 2 * scale, 3 * scale }, { 4 * scale, 5 * scale, 6 * scale } };
+            this.assertPixelValuesEqual(expected, norm);
+        }
+
+        {
+            GrayscaleImage norm = this.smallBright.normalized();
+            double scale = 127 / 253.5;
+            double[][] expected = new double[][]{ { 255 * scale, 254 * scale }, { 253 * scale, 252 * scale } };
+            this.assertPixelValuesEqual(expected, norm);
+        }
+
+        {
+            GrayscaleImage norm = this.smallMedium.normalized();
+            double scale = 127 / 128.5;
+            double[][] expected = new double[][]{ { 127 * scale, 128 * scale }, { 129 * scale, 130 * scale } };
+            this.assertPixelValuesEqual(expected, norm);
+        }
+    }
+
+    @Test
+    public void testInvert() {
+        {
+            this.smallSquare.invert();
+            double[][] expected = new double[][]{ { 254, 253 }, { 252, 251 } };
+            this.assertPixelValuesEqual(expected, this.smallSquare);
+        }
+
+        {
+            this.smallWide.invert();
+            double[][] expected = new double[][]{ { 254, 253, 252 }, { 251, 250, 249 } };
+            this.assertPixelValuesEqual(expected, this.smallWide);
+        }
+
+        {
+            this.smallBright.invert();
+            double[][] expected = new double[][]{ { 0, 1 }, { 2, 3 } };
+            this.assertPixelValuesEqual(expected, this.smallBright);
+        }
+
+        {
+            this.smallMedium.invert();
+            double[][] expected = new double[][]{ { 128, 127 }, { 126, 125 } };
+            this.assertPixelValuesEqual(expected, this.smallMedium);
+        }
+    }
+
+    @Test
+    public void testMirrored() {
+        {
+            double[][] expected = new double[][]{ { 2, 1 }, { 4, 3 } };
+            GrayscaleImage.mirrored(this.smallSquare);
+            this.assertPixelValuesEqual(expected, this.smallSquare);
+        }
+
+        {
+            double[][] expected = new double[][]{ { 3, 2, 1 }, { 6, 5, 4 } };
+            GrayscaleImage.mirrored(this.smallWide);
+            this.assertPixelValuesEqual(expected, this.smallWide);
+        }
+
+        {
+            double[][] expected = new double[][]{ { 254, 255 }, { 252, 253 } };
+            GrayscaleImage.mirrored(this.smallBright);
+            this.assertPixelValuesEqual(expected, this.smallBright);
+        }
+
+        {
+            double[][] expected = new double[][]{ { 128, 127 }, { 130, 129 } };
+            GrayscaleImage.mirrored(this.smallMedium);
+            this.assertPixelValuesEqual(expected, this.smallMedium);
+        }
+    }
+
 }
